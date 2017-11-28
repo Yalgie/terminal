42d158d4fc791a1f606be11ddaf3f6a651a305d0
Jamie Logan <jamie.logan@webslinger.com.au>
2017-11-28 16:46:13 +1000

    Re-work of JSON elements
    Moderate edit, re-worked hidden content as well as usernames/passwords/inputs so they're all stored via one single JSON file as opposed to single variables and separate files.
    Minor edit to main index file, added more keywords to meta tags, moved inline JS to main terminal.js file.
    Removed redundant JSON files.

--- js/terminal.js
+++ js/terminal.js
@@ -1,4 +1,9 @@
 $(function() {
+    var currentYear = new Date().getFullYear();
+    $('.this_year').html(currentYear);
+});
+
+$(function() {
     var $terminal = $("#terminal");
     var $cmd = $terminal.find(".typing");
     var $cmdPrefix = $terminal.find(".cmdPrefix");
@@ -6,20 +11,31 @@ $(function() {
     var valid = true;
     var user = false;
     var pass = false;
-    var username = "dave";
-    var password = "grimrasputinisdead";
-    var anagram = "paradigmindustries";
     
-    function renderLines(data) {
+    var daveData = (function () {
+        var daveData = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "http://tw.dave/js/whathappenedtodave.json", // Don't forget to change this to updated URL once pushed to live
+            'dataType': "json",
+            'success': function (data) {
+                daveData = data;
+            }
+        });
+        return daveData;
+    })();
+    
+    function renderLines(daveData) {
         var iCount = 0;
         
-        function renderText(data) {
-            $text.append(data.text[iCount])
+        function renderText(daveData) {
+            $text.append(daveData[iCount])
 
-            if (iCount < data.text.length) {
+            if (iCount < daveData.length) {
                 setTimeout(function(){
                     iCount++;
-                    renderText(data);
+                    renderText(daveData);
                     $terminal.scrollTop($terminal[0].scrollHeight);
                 }, 200);
             } else {
@@ -31,16 +47,14 @@ $(function() {
         }
         
         if (valid) {
-            setTimeout(function(){renderText(data);}, 3300);
+            setTimeout(function(){renderText(daveData);}, 3300);
         } else {
-            setTimeout(function(){renderText(data);}, 200);
+            setTimeout(function(){renderText(daveData);}, 200);
         }
     };
 
-    function init(data) {
-        $.getJSON( "boot_text.json", function(data) {
-            renderLines(data);
-        });
+    function init(daveData) {
+        renderLines(daveData[0].bootText[0].text);
     };
 
     function bindTerminalEvents() {
@@ -72,109 +86,101 @@ $(function() {
         if (($cmd.text() != undefined) && valid) {
             var cmd = $cmd.text().toLowerCase();
 
-            if (pass && cmd == anagram) {
+            if (pass && cmd == (daveData[0].anagram)) {
                 valid = false;
                 $text.append("For 2-step authentication, please rearrange the following letters into correct order<br/>GRIMRASPUTINISDEAD -> " + cmd + "<br/>")
                 $text.append("<span class='correct'>Anagram Accepted</span> <br/><br/><br/>");
-                $cmd.html("")
-                $cmdPrefix.html("")
-                showSecretData01()
+                $cmd.html("");
+                $cmdPrefix.html("");
+                showSecretData01();
             }
             else if (pass) {
-                $text.append("For 2-step authentication, please rearrange the following letters into correct order<br/>GRIMRASPUTINISDEAD -> " + cmd + "<br/>")
+                $text.append("For 2-step authentication, please rearrange the following letters into correct order<br/>GRIMRASPUTINISDEAD -> " + cmd + "<br/>");
                 $text.append("<span class='incorrect'>Anagram Incorrect</span> <br/>");
                 $cmdPrefix.html("Please try again... ->");
             };
 
-            if (!user && cmd == username) {
+            if (!user && cmd == (daveData[0].username)) {
                 user = true;
                 $text.append("Username: " + cmd + "<br/>")
-                $text.append("<span class='correct'>Username Recognized</span><br/><br/>");
+                $text.append("<span class='correct'>Username Recognized</span><br/><br/>")
                 $cmdPrefix.html("Enter Password:")
             }
-			else if (user && cmd == password) {
+			else if (user && cmd == (daveData[0].password)) {
                 pass = true;
-                $text.append("Enter Password: " + cmd + "<br/>")
+                $text.append("Enter Password: " + cmd + "<br/>");
                 $text.append("<span class='correct'>Password Accepted</span><br/><br/>");
-                $cmdPrefix.html("For 2-step authentication, please rearrange the following letters into correct order<br/>GRIMRASPUTINISDEAD ->")
+                $cmdPrefix.html("For 2-step authentication, please rearrange the following letters into correct order<br/>GRIMRASPUTINISDEAD ->");
             }
             else if (!pass) {
                 if (user) {
-                    $text.append("Enter Password: " + cmd + "<br/>")
-                    $text.append("<span class='incorrect'>Password Incorrect</span> <br/>")
-                    $cmdPrefix.html("Enter Password:")
+                    $text.append("Enter Password: " + cmd + "<br/>");
+                    $text.append("<span class='incorrect'>Password Incorrect</span> <br/>");
+                    $cmdPrefix.html("Enter Password:");
                 }
                 else {
-                    $text.append("Username: " + cmd + "<br/>")
-                    $text.append("<span class='incorrect'>Username Incorrect</span> <br/>")
-                    $cmdPrefix.html("Username:")
+                    $text.append("Username: " + cmd + "<br/>");
+                    $text.append("<span class='incorrect'>Username Incorrect</span> <br/>");
+                    $cmdPrefix.html("Username:");
                 }
             };
 
-            $cmd.html("")
+            $cmd.html("");
             $terminal.scrollTop($terminal[0].scrollHeight);
         };
     };
 
     function showSecretData01(e) {
-        $.getJSON( "secretData01.json", function(data) {
-            renderLines(data);
-            $terminal.focus();
-		
-            $terminal.one("keydown", function(e) {
-                if (e.which == 13) {
-                    e.preventDefault();
-                    showSecretData02();
-                };
-            });
-        });
-        
+        renderLines(daveData[0].secretData[0].secretData_1.text);
         $cmd.hide();
+        
+        if(event.keyCode == 13) event.stopPropagation();
+
+        $terminal.focus();
+        
+        $terminal.one("keydown", function(e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                showSecretData02();
+            };
+        });
     }
     
     function showSecretData02(e) {
-        $.getJSON( "secretData02.json", function(data) {
-            renderLines(data);
-            
-            $terminal.one("keydown", function(e) {
-                if (e.which == 13) {
-                    e.preventDefault();
-                    showSecretData03();
-                };
-            });
+        renderLines(daveData[0].secretData[1].secretData_2.text);
+
+        $terminal.one("keydown", function(e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                showSecretData03();
+            };
         });
     }
     
     function showSecretData03(e) {
-        $.getJSON( "secretData03.json", function(data) {
-            renderLines(data);
-            
-            $terminal.one("keydown", function(e) {
-                if (e.which == 13) {
-                    e.preventDefault();
-                    showSecretData04();
-                };
-            });
+        renderLines(daveData[0].secretData[2].secretData_3.text);
+
+        $terminal.one("keydown", function(e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                showSecretData04();
+            };
         });
     }
     
     function showSecretData04(e) {
-        $.getJSON( "secretData04.json", function(data) {
-            renderLines(data);
-            
-            $terminal.one("keydown", function(e) {
-                if (e.which == 13) {
-                    e.preventDefault();
-                    showSecretData05();
-                };
-            });
+        renderLines(daveData[0].secretData[3].secretData_4.text);
+
+        $terminal.one("keydown", function(e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                showSecretData05();
+            };
         });
     }
     
     function showSecretData05(e) {
-        $.getJSON( "secretData05.json", function(data) {
-            renderLines(data);
-        });
+        renderLines(daveData[0].secretData[4].secretData_5.text);
     }
     
     function customConsoleLog(message, bg, color) {
@@ -184,5 +190,5 @@ $(function() {
     customConsoleLog("'What Happened to Dave?' Terminal // Developed by 2Wolves", "#1B1632", "#A292E7");
     customConsoleLog("Visit us @ http://2wolves.io", "#1B1632", "#A292E7");
 
-    init();
+    init(daveData);
 });
